7efb53c33e21a371ccefd0123ef5abbc0d892666
Orbter <assouline39@gmail.com>
2024-01-29 23:49:05 +0200

    updated the js and started to create a dom

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const gameBoard = [
-  ["X", "O", "X"],
-  ["O", "O", "X"],
-  ["X", "O", "O"],
+  [, , ,],
+  [, , ,],
+  [, , ,],
 ];
 console.log(gameBoard);
-
-const User = function (user, opponent) {
-  this.user = user;
-  this.opponent = opponent;
-};
 
 function matrixLength(board) {
   const numberOfRows = board.length;
@@ -43,6 +38,7 @@ function checkColumns(board) {
   let sameInLine = true;
   let { numberOfRows, numberOfColumns } = matrixLength(board);
   for (let i = 0; i < numberOfColumns; i++) {
+    sameInLine = true;
     for (let k = 0; k < numberOfRows - 1; k++) {
       let currentNumber = board[k][i];
       let nextNumber = board[k + 1][i];
@@ -58,8 +54,53 @@ function checkColumns(board) {
   }
   return isWin;
 }
+function diagonal(board) {
+  let { numberOfRows, numberOfColumns } = matrixLength(board);
+  let isWin = true;
 
-function checkIfWin() {}
+  // Check top-left to bottom-right diagonal
+  let currentNumber = board[0][0];
+  for (let i = 1; i < numberOfRows && i < numberOfColumns; i++) {
+    if (!currentNumber || board[i][i] !== currentNumber) {
+      isWin = false;
+      break;
+    }
+  }
+
+  if (isWin) {
+    return true;
+  }
+
+  // Check top-right to bottom-left diagonal
+  isWin = true;
+  currentNumber = board[0][numberOfColumns - 1];
+  for (let i = 1; i < numberOfRows && i < numberOfColumns; i++) {
+    if (!currentNumber || board[i][numberOfColumns - 1 - i] !== currentNumber) {
+      isWin = false;
+      break;
+    }
+  }
+
+  return isWin;
+}
+
+function checkIfWin(board) {
+  if (
+    diagonal(board) === true ||
+    checkColumns(board) === true ||
+    checkRows(board) === true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkIfWin(gameBoard));
+
+const User = function (user, opponent) {
+  this.user = user;
+  this.opponent = opponent;
+};
 
 function playersChoice(player1, player2) {
   User(player1, player2);
